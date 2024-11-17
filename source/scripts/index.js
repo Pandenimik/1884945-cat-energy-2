559bcb867d2eb0245660c1bc2bc54f0627c2bfe7
Pandenimik <89628864+Pandenimik@users.noreply.github.com>
2024-11-17 21:41:03 +0300

    Подключает основные иконки.

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,11 @@
-/* в этот файл добавляет скрипты*/
+const button = document.querySelector('.burger-toggle');
+const navList = document.querySelector('.main-nav__list');
+const toggleNav = function () {
+  navList.classList.toggle('main-nav__list--closed');
+  button.classList.toggle('opened-nav');
+};
+
+button.addEventListener('click', (e) => {
+  e.stopPropagation();
+  toggleNav();
+});
